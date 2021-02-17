1ac50fcad0037b6cb8c700bb5122313cd4ee449a
Velin Klisurov <vtklisurov@gmail.com>
2021-02-17 18:45:28 +0200

    highlight almost working - closing loops breaks it

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ enum Instruction {
     Decrement,
     Write,
     Read,
-    Loop(Vec<Instruction>)
+    Loop(Vec<InstructionIndex>)
 }
 
 #[derive(Debug)]
@@ -50,7 +50,15 @@ enum Action {
 struct CellChange{
     index: usize,
     content: u8,
-    action: Action
+    action: Action,
+    text_index: usize
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct InstructionIndex{
+    index: usize,
+    code: Instruction,
 }
 
 /// Lexer turns the source code into a sequence of opcodes
@@ -80,47 +88,93 @@ fn lex(source: String) -> Vec<OpCode> {
     operations
 }
 
-fn parse(opcodes: Vec<OpCode>) -> Vec<Instruction> {
-    let mut program: Vec<Instruction> = Vec::new();
+fn parse(opcodes: Vec<OpCode>, txt_index: &mut usize) -> Vec<InstructionIndex> {
+    let mut program: Vec<InstructionIndex> = Vec::new();
     let mut loop_stack = 0;
     let mut loop_start = 0;
 
     for (i, op) in opcodes.iter().enumerate() {
         if loop_stack == 0 {
             let instr = match op {
-                OpCode::IncrementPointer => Some(Instruction::IncrementPointer),
-                OpCode::DecrementPointer => Some(Instruction::DecrementPointer),
-                OpCode::Increment => Some(Instruction::Increment),
-                OpCode::Decrement => Some(Instruction::Decrement),
-                OpCode::Write => Some(Instruction::Write),
-                OpCode::Read => Some(Instruction::Read),
+                OpCode::IncrementPointer => { 
+                    println!("increment pointer");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::IncrementPointer})                 
+                },
+                OpCode::DecrementPointer => {
+                    println!("decrement pointer");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::DecrementPointer})
+                },
+                OpCode::Increment => {
+                    println!("increment data");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::Increment})
+                },
+                OpCode::Decrement => {
+                    println!("decrement data");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::Decrement})
+                },
+                OpCode::Write => {
+                    println!("write");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::Write})
+                },
+                OpCode::Read => {
+                    println!("read");
+                    println!("{:?}", *txt_index);
+                    *txt_index += 1;
+                    Some(InstructionIndex{index: *txt_index-1, code: Instruction::Read})
+                },
 
                 OpCode::LoopBegin => {
+                    println!("loop begin");
+                    println!("{:?}", *txt_index);
                     loop_start = i;
                     loop_stack += 1;
                     None
                 },
 
-                OpCode::LoopEnd => panic!("loop ending at #{} has no beginning", i),
+                OpCode::LoopEnd => {
+                    println!("error loop");
+                    println!("{:?}", *txt_index);
+                    panic!("loop ending at #{} has no beginning", i)
+                },
             };
 
-            match instr {
+            match instr{
                 Some(instr) => program.push(instr),
                 None => ()
             }
         } else {
             match op {
                 OpCode::LoopBegin => {
+                    println!("nested loop begin");
+                    println!("{:?}", *txt_index);
                     loop_stack += 1;
                 },
                 OpCode::LoopEnd => {
+                    println!("nested loop end");
+                    println!("{:?}", *txt_index);
+
                     loop_stack -= 1;
 
                     if loop_stack == 0 {
-                        program.push(Instruction::Loop(parse(opcodes[loop_start+1..i].to_vec())));
+                        println!("loop end");
+                        println!("{:?}", *txt_index);
+                        *txt_index += 1;
+                        program.push(InstructionIndex{index: i, code: Instruction::Loop(parse(opcodes[loop_start+1..i].to_vec(), txt_index))});
                     }
                 },
-                _ => (),
+                _ => {
+                    *txt_index-=1;
+                },
             }
         }
     }
@@ -133,46 +187,46 @@ fn parse(opcodes: Vec<OpCode>) -> Vec<Instruction> {
 }
 
 /// Executes a program that was previously parsed
-fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut usize, send_cell: std::sync::mpsc::Sender<CellChange>) {
+fn run(instructions: &Vec<InstructionIndex>, tape: &mut Vec<u8>, data_pointer: &mut usize, send_cell: std::sync::mpsc::Sender<CellChange>) {
     for instr in instructions {
-        thread::sleep(time::Duration::from_millis(10));
+        thread::sleep(time::Duration::from_millis(100));
         match instr {
-            Instruction::IncrementPointer => {
-                println!("increment pointer");
+            InstructionIndex{index: i, code: Instruction::IncrementPointer} => {
+                
                 *data_pointer += 1;
-                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape});
+                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape, text_index: *i});
             },
-            Instruction::DecrementPointer => {
-                println!("decrement pointer");
+            InstructionIndex{index: i, code: Instruction::DecrementPointer} => {
+                
                 *data_pointer -= 1;
-                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape});
+                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape, text_index: *i});
             },
-            Instruction::Increment => {
-                println!("increment data");
+            InstructionIndex{index: i, code: Instruction::Increment} => {
+                
                 tape[*data_pointer] += 1;
-                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape});
+                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape, text_index: *i});
             },
-            Instruction::Decrement => {
-                println!("decrement data");
+            InstructionIndex{index: i, code: Instruction::Decrement} => {
+                
                 tape[*data_pointer] -= 1;
-                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape});
+                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Tape, text_index: *i});
             },
-            Instruction::Write => {
-                println!("write");
-                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Output});
+            InstructionIndex{index: i, code: Instruction::Write} => {
+                
+                send_cell.send(CellChange{index: *data_pointer, content: tape[*data_pointer], action: Action::Output, text_index: *i});
             },
 
             //FIX THIS ------------------------------------------------------------------------------------------------------------------------------//
-            Instruction::Read => {
-                println!("read");
+            InstructionIndex{index: i, code: Instruction::Read} => {
+                
                 todo!();
                 let mut input: [u8; 1] = [0; 1];
                 std::io::stdin().read_exact(&mut input).expect("failed to read stdin");
                 tape[*data_pointer] = input[0];
             },
             //---------------------------------------------------------------------------------------------------------------------------------------//
-            Instruction::Loop(nested_instructions) => {
-                println!("entered loop");
+            InstructionIndex{index: i, code: Instruction::Loop(nested_instructions)} => {
+                
                 while tape[*data_pointer] != 0 {
                     println!("looping");
                     run(&nested_instructions, tape, data_pointer, send_cell.clone())
@@ -183,13 +237,15 @@ fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut u
 }
 
 fn main() {
+
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
+
+
     let glade_src = include_str!("../GUI.glade");
     let builder = gtk::Builder::from_string(glade_src);
-    
     let window: gtk::Window = builder.get_object("window").unwrap();
     let start_button: gtk::Button= builder.get_object("btnStart").unwrap();
     let pause_button: gtk::Button = builder.get_object("btnPause").unwrap();
@@ -215,13 +271,16 @@ fn main() {
             marker_lbls[i].set_text("");
         }
 
+        let mut txt_index:usize = 0;
+
         let buf: gtk::TextBuffer = output.get_buffer().unwrap();
+        let in_buf: gtk::TextBuffer = input.get_buffer().unwrap();
         buf.set_text("");
         output.set_buffer(Some(&buf));
         let source_buffer = input.get_buffer().unwrap();
         let source = source_buffer.get_text(&source_buffer.get_start_iter(), &source_buffer.get_end_iter(), false);
         let opcodes = lex(String::from(source.as_ref().unwrap().as_str()));
-        let program = parse(opcodes);
+        let program = parse(opcodes, &mut txt_index);
         let mut tape: Vec<u8> = vec![0; 32];
         let mut data_pointer = 0;
 
@@ -251,22 +310,25 @@ fn main() {
             //     }
             // }
             match received.clone(){
-                Ok(CellChange{index: _, content: _, action: Action::Output}) => {
+                Ok(CellChange{index: _, content: _, action: Action::Output, text_index: i}) => {
                     println!("{:?}", received);
                     let mut output = String::from(buf.get_text(&buf.get_start_iter(), &buf.get_end_iter(), false).unwrap().as_str());
+                    in_buf.select_range(&in_buf.get_iter_at_offset(i as i32), &in_buf.get_iter_at_offset(i as i32 +1));
+
                     output.push(received.unwrap().content as char);
                     buf.set_text(output.as_str());
                     while gtk::events_pending(){
                         gtk::main_iteration();
                     }
                 }
-                Ok(_) =>{
+                Ok(CellChange{index: _, content: _, action: Action::Tape, text_index: i}) => {
                     println!("{:?}", received);
                     for i in 0..32 {
                         marker_lbls[i].set_text("");
                     }
                     marker_lbls[received.clone().unwrap().index].set_text("#");
                     let tmp = received.clone().unwrap().content.to_string();
+                    in_buf.select_range(&in_buf.get_iter_at_offset(i as i32), &in_buf.get_iter_at_offset(i as i32 +1));
                     tape_lbls[received.clone().unwrap().index].set_text(&tmp[..]);
                     while gtk::events_pending(){
                         gtk::main_iteration();
